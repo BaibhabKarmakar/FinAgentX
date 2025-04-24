34fe45eb47b3309abd4a1c167487f8aa4c810472
baibhabkarmakar <baibhabkarmakar29@gmail.com>
2025-04-24 12:17:50 +0530

    Added MArketDataAgent with live Finnhub inegration

--- agents/market_agent.py
+++ agents/market_agent.py
@@ -1,7 +1,35 @@
+import requests
+import os
+from dotenv import load_dotenv
 from core.utils import BaseAgent
 
+load_dotenv()
+
 class MarketDataAgent(BaseAgent):
-    def run(self , input_data):
-        # placeholder: Simulate market data retrieval
-        print(f"{self.name}: Fetching market Data...")
-        return {"amrket_data": "sample data"}
+    def __init__(self):
+        super().__init__("MarketDataAgent")
+        self.api_key = os.getenv("FINHUB_API_KEY")
+        self.base_url = "https://finnhub.io/api/v1"
+
+    def run(self , symbol="AAPL"):
+        endpoint = f"{self.base_url}/quote"
+        params = {
+            "symbol": symbol,
+            "token": self.api_key
+        }
+        try:
+            response = requests.get(endpoint , params=params)
+            data = response.json()
+            if "c" in data:
+                return {
+                    "symbol": symbol,
+                    "current_price": data["c"],
+                    "high": data["h"],
+                    "low": data["l"],
+                    "open": data["o"],
+                    "previous_close": data["pc"]
+                }
+            else:
+                return {"error": "Invalid symbol or API limit reached!"}
+        except Exception as e:
+            return {"error": str(e)}
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import streamlit as st
-st.title("FinAgentX")
-st.write("Hello, Baibhab! This is your AI finance Agent in the making!")
+from agents.market_agent import MarketDataAgent
+
+def test_market_agent():
+    symbols = ["AAPL" , "GOOGL" , "TSLA" , "BINANCE:BTCUSDT" , "NFLX"]
+    agent = MarketDataAgent()
+
+    for sym in symbols:
+        data = agent.run(sym)
+        print(f"{sym}:")
+        print(data)
+        print("\n")
+
+if __name__ == "__main__":
+    test_market_agent()
